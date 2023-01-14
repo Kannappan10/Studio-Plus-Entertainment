ee28f0b1bd3e9e7325ae09308989c44aedbdc85f
Kannappan10 <117585401+Kannappan10@users.noreply.github.com>
2023-01-14 13:47:41 +0530

    Update horizontalgrid.vue

--- components/horizontalgrid.vue
+++ components/horizontalgrid.vue
@@ -48,8 +48,8 @@ export default {
         return {
             alldetails: [],
             details: [],
-            api_key: "AIzaSyCkzCAGvVt7fKE_un1wHKvy2NLns86p3jQ",
-            // api_key: "AIzaSyB6CbQR3WNUzTnuTg5iPvwNDfAqmdXjfII", // Another API key
+            // api_key: "AIzaSyCkzCAGvVt7fKE_un1wHKvy2NLns86p3jQ", //
+             api_key: "AIzaSyB6CbQR3WNUzTnuTg5iPvwNDfAqmdXjfII", 
             // api_key: "AIzaSyBIEWv1CI8xRwkfqVxJhv48edsuR-OjHiQ", // Another API key
 
         }
